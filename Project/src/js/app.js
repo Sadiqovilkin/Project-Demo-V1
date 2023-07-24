21886fa329ffc49026c2f32e6836374282928c61
Sadiqovilkin <sadiqovilkin068@gmail.com>
2023-07-24 12:29:00 +0400

    #finished slider and bgimg section

--- Project/src/js/app.js
+++ Project/src/js/app.js
@@ -5,3 +5,27 @@ if (btn_watchlist) {
         btn_element.classList.toggle("el_active")
     })  
 }
+
+
+
+// slider js
+var swiper = new Swiper(".mySwiper", {
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+    breakpoints: {
+        640: {
+          slidesPerView: 2,
+          spaceBetween: 20,
+        },
+        768: {
+          slidesPerView: 4,
+          spaceBetween: 40,
+        },
+        1024: {
+          slidesPerView: 4.2,
+          spaceBetween: 25,
+        },
+      },
+  });
